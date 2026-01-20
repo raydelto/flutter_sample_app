bbb37fa980b79effccb5f55a6736b20d3f84da36
Raydelto Hernandez <raydelto@gmail.com>
2026-01-20 10:02:35 -0400

    introduce ternary operator for IconData on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -139,12 +139,9 @@ class GeneratorPage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
+    IconData icon = appState.favorites.contains(pair)
+        ? Icons.favorite
+        : Icons.favorite_border;
 
     return Center(
       child: Column(
